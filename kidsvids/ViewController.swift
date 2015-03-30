f77e619a0197d7bacc0ec51f18d96d6c44da44ca
Rehan Ali <ali.bobby@gmail.com>
2015-03-30 11:33:07 +0100

    Initial Commit

---
+++ kidsvids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kidsvids
+//
+//  Created by Bobby on 30/03/2015.
+//  Copyright (c) 2015 Azuki Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
